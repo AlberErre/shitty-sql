078557762edd1a82b7308fb762f54ef037347787
Alber Erre <arblugo@gmail.com>
2018-11-19 19:35:10 +0000

    Update series_de_ambos_clare_y_luke.sql

--- series_de_ambos_clare_y_luke.sql
+++ series_de_ambos_clare_y_luke.sql
@@ -1,10 +1,12 @@
 select SERIES.TITLE 
 from CHARACTERS_SERIES, CHARACTERS, SERIES 
-where CHARACTERS_SERIES.id_character = (( SELECT CHARACTERS.ID
-											from CHARACTERS
-											where CHARACTERS.NAME = 'luke')
-	OR                  				( SELECT CHARACTERS.ID
-											from CHARACTERS
-											where CHARACTERS.NAME = 'clare'))
+where CHARACTERS_SERIES.id_character = (
+( SELECT CHARACTERS.ID
+  from CHARACTERS
+  where CHARACTERS.NAME = 'luke')
+OR 
+( SELECT CHARACTERS.ID
+  from CHARACTERS
+  where CHARACTERS.NAME = 'clare'))
 and CHARACTERS_SERIES.id_character = CHARACTERS.ID
 and CHARACTERS_SERIES.id_serie = SERIES.ID;
